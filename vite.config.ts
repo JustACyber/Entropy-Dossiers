9ec7774383c435f6f7be49f19cfdfc77ab284eb4
JustACyber <fedorderduga@gmail.com>
2026-02-17 02:50:54 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Critical for GitHub Pages to load assets correctly
   build: {
     outDir: 'dist',
     sourcemap: false
